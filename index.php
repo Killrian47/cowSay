80eaa634c364122db3acfbc27e0e871bfdf9f3b6
salut <killian.portier@yahoo.fr>
2022-10-21 15:04:45 +0200

    say-cow

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require 'src/Cow.php';
+$cow = new \App\Cow();
+echo $cow->say();
---
+++ src/Cow.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Cow
+{
+    public function say(): string
+    {
+        return 'Hello';
+    }
+}
